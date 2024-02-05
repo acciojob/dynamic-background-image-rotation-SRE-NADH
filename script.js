8ef58050b99d4148b859273ea53ad769a7f41829
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2024-02-06 02:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let body = document.getElementsByTagName('body');
+let orientation = window.screen.orientation.type;
+if(orientation.toLowerCase().includes("landscape")){
+	body[0].style.backGroundImage ="https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg"
+}
+else{
+	body[0].style.backGroundImage = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg"
+}
+
+
